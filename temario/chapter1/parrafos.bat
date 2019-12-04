cb6b482be8c7db73dc7031132ee97d2874278e05
ARIEL <carraro.ariel@gmail.com>
2019-12-03 19:33:06 -0600

    parrafos.bat ha sido creado

---
+++ temario/chapter1/parrafos.bat
@@ -0,0 +1,17 @@
+@echo off
+TITLE PARRAFOS
+::Fondo rojo, texto blanco
+color 4f
+::limpiar pantalla
+cls
+
+:PRINCIPAL
+echo ********************************
+echo            CMD
+echo ********************************
+GOTO PARRAFO1
+
+:PARRAFO1
+echo Adios!!
+
+PAUSE
