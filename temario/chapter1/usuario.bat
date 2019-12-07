5ac260d6523a12fe45fc10d5476f108d0fc240c2
ARIEL <carraro.ariel@gmail.com>
2019-12-06 21:56:15 -0600

    usuario.bat creado
    Archivo para mostrar el nombre del usuario

---
+++ temario/chapter1/usuario.bat
@@ -0,0 +1,17 @@
+@echo off 
+
+echo Programando en Batch
+pause 
+
+GOTO Comentario
+
+ -- esto es
+
+   un comentario multi línea
+
+que no se interpretará en MS-DOS
+
+ --
+
+:Comentario
+echo Hola %USERNAME%
