dae8abf67ea2654df2ef77c79ca8ee58b89c1b9c
ARIEL <carraro.ariel@gmail.com>
2020-02-15 08:25:08 -0600

    Comandos básicos Windows

---
+++ temario/chapter1/comandos.bat
@@ -0,0 +1,11 @@
+@echo off
+TITLE Ejemplos de comandos
+echo Uso de comando ping
+PING www.google.com
+echo Uso de comando systeminfo
+SYSTEMINFO
+echo Uso de comando tree
+TREE
+pause
+::Salimos
+exit
