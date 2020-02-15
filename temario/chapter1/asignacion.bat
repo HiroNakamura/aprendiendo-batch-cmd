4af0d1da0f3c06464d3f74d547ede51e6cd881c2
ARIEL <carraro.ariel@gmail.com>
2020-02-15 08:17:22 -0600

    Se ha editado asignacion.bat
    Se edito set /a cadena por set cadena

--- temario/chapter1/asignacion.bat
+++ temario/chapter1/asignacion.bat
@@ -1,14 +1,20 @@
 @echo off
 TITLE USO DE SET
+COLOR 4F
 
-:Asignar valores a variables
+::Asignar valores a variables
 SET /a x = 21
 SET /a y = 35
 SET /a suma = x+y
-SET /a cadena = El libro de los Despertares.
+SET cadena = El libro de los Despertares.
 
 echo Suma: %suma%
 echo %cadena%
 
+echo Tu nombre:
+set /p nombre=
+echo.
+echo Ciao %nombre%
+COLOR
 @REM pause para pausar el programa
 pause
